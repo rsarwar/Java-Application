1da9b7a495febb88a22414e7b643356892d628c3
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2018-01-18 12:49:39 -0500

    here you go rammy :)

--- All Around the World of Africa/src/CSVUtilities.java
+++ All Around the World of Africa/src/CSVUtilities.java
@@ -1,8 +1,119 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class CSVUtilities {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.scene.text.*;
 
-	public CSVUtilities() {
-		// TODO Auto-generated constructor stub
+public class CSVUtilities
+{
+	private final int seconds = 10;
+	ArrayList<String> CSVWorldData = new ArrayList<String>();
+	ArrayList<String> CSVUserData = new ArrayList<String>();
+	int numColumns = 0;
+	int numRows = 0;
+	int score = 0;
+	String name = "";
+	
+	
+	public void CSVUtilities(File csv)
+	{
+			FileReader reader = null;
+			BufferedReader filein = null;
+			try
+			{
+				reader = new FileReader(csv);
+				filein = new BufferedReader(reader);
+				try
+				{
+					String line = filein.readLine();
+					
+						while (line != null)
+						{ 						
+							String[] attributes = line.split(",");
+							numColumns = attributes.length;
+							for(int x = 0; x < numColumns; x++ )
+							{
+								CSVData.add(attributes[x]);
+							}
+							numRows++;
+							line = filein.readLine(); 
+						}
+				}
+				
+				catch (IOException ioe)
+				{
+		            ioe.printStackTrace();
+		        }	
+			}
+
+		catch (FileNotFoundException fnfe)
+		{
+			System.out.println("ERROR: File not found!");
+		}
+
+	}
+	
+	public void writeCSV(File file)
+	{
+		PrintWriter pw = null;
+		try
+		{
+			pw = new PrintWriter(file);
+		}
+		catch(FileNotFoundException e)
+		{
+			System.err.println(e);
+		}
+		StringBuilder sb = new StringBuilder();
+		
+		/*
+		sb.append("Name");
+        sb.append(',');
+        sb.append("High Score");
+        sb.append('\n');
+        */
+        
+        
+        sb.append(this.name);
+        sb.append(',');
+        sb.append(this.score);
+        sb.append('\n');
+        
+        pw.write(sb.toString());
+        pw.close();
+        //System.out.println("done!");
+	}
+	
+	public void printData()
+	{
+		int y = this.numColumns;
+		int z = this.numRows;
+		int a = 0;
+		int b = y * z;
+		while(a < b)
+		{
+			for(int i = 0; i < y; i++)
+			{	
+				System.out.print(this.CSVData.get(a) + "  ");
+				a++;
+			}
+			System.out.println();
+			
+		}
 	}
 
+	
 }
