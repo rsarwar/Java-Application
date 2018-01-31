8c98e4847f8123afa7e4b9d857bb7be70e635f82
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2018-01-31 13:10:38 -0500

    mitm

--- All Around the World of Africa/src/NorthRegion.java
+++ All Around the World of Africa/src/NorthRegion.java
@@ -1,5 +1,4 @@
 
-
 import java.util.Scanner;
 
 public class NorthRegion {
@@ -7,7 +6,11 @@ public class NorthRegion {
 	private Scanner input;
 	public String greetUser()
 	{
-		System.out.println("");
-		return "";
+		System.out.println("Welcome to the North region of Africa! I see you've met Leyla :D I'm Brendon :)");
+		//input = new Scanner(System.in);
+		
+		return "There are 7 different countries that can be found in this region.";
 	}
+	
+	NTunisia chatbot1 = new NTunisia();
 }
--- All Around the World of Africa/src/TourRunner.java
+++ All Around the World of Africa/src/TourRunner.java
@@ -8,21 +8,46 @@
 	//http://pitt.libguides.com/c.php?g=12378&p=65814 USEFUL!! 
 public class TourRunner {
 		
+	public String name = "";
 		public static void main(String[] args)
 		{
 			System.out.println("Welcome to Africa! I'm Leyla and I'll be your tour guide :) What's your name? ");
 			
 			in = new Scanner(System.in);
-			
+
+			String name = in.next();
 			String statement = in.nextLine();
-			System.out.println("Nice to meet you " + statement + "!");
+			System.out.println("Nice to meet you " + name + "!");
 			System.out.println("Did you know that Africa is broken down into 5 regions?");
+			
+			
 			statement = in.nextLine();
 
-			if(statement.equals("yes"))
+			
+			
+			
+			if(statement.toLowerCase().equals("yes"))
 			{
 				System.out.println("Nice! What region would you like to talk about?");
+				statement = in.nextLine();
+				
+				
+				if(statement.toLowerCase().equals("north"))
+				{
+					NorthRegion chatbot1 = new NorthRegion();
+					System.out.println(chatbot1.greetUser());
+					
+					/*
+					while(!statement.equals("Bye"))
+					{
+						System.out.println(chatbot2.);
+					}
+					*/
+				}
+			
 			}
+			
+			
 			if(statement.equals("no"))
 			{ 
 				for(String str: regions)
@@ -31,17 +56,12 @@ public class TourRunner {
 				}
 			}
 			
-			NorthRegion chatbot1 = new NorthRegion();
 			EastRegion chatbot2 = new EastRegion();
 			CentralRegion chatbot3 = new CentralRegion();
 			WestRegion chatbot4 = new WestRegion();
 			SouthRegion  chatbot5 = new SouthRegion();
 
 			/*
-			public String getResponse(String statement)
-			{
-				
-			}			 
 			if(statement.equals("yes"))
 			{
 				for(String str: countries)
@@ -54,10 +74,10 @@ public class TourRunner {
 		
 		 static String[] regions = {"North", "East", "Central", "West", "South"};
 		 static String[] northC = {"Algeria", "Egypt", "Libya", "Morocco", "Sudan", "Tunisia", "Western Sahara"};
-		 static String[] eastC = {"Burundi", "Comoros", "Djibouti", "Eritrea", "Ethiopia", "Kenya", "Madagascar", "Malawi", "Mauritius", "Réunion"};
-		 static String[] centralC = {};
-		 static String[] westC = {}; 
-		 static String[] southC = {};
+		 static String[] eastC = {"Burundi", "Comoros", "Djibouti", "Eritrea", "Ethiopia", "Kenya", "Madagascar", "Malawi", "Mauritius", "Réunion", "Rwanda", "Seychelles", "Somalia", "Somaliland", "Tanzania", "Uganda"};
+		 static String[] centralC = {"Cameroon", "Central African Republic", "Chad", "Congo Republic - Brazzaville", "Democratic Republic of Congo", "Equatorial Guinea", "Gabon","" , "São Tomé & Principe"};
+		 static String[] westC = {"Benin", "Burkina Faso", "Cape Verde", "Côte D'Ivoire", "Gambia", "Ghana", "Guinea", "Guinea-Bissau", "Liberia", "Mali", "Mauritania", "Niger", "Nigeria", "Saint Helena", "Senegal", "Sierra Leone", "Togo"};
+		 static String[] southC = {"Angola", "Botswana", "Lesotho", "Mozambique", "Namibia", "South Africa", "Swaziland", "Zambia", "Zimbabwe"}; 
 		 static String[] countries = {"Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cabo Verde", "Cameroon", "Central African Republic (CAR)", "Chad", "Comoros", "Democratic Republic of Congo", "Republic of Congo", "Cote d'Ivoire", "Djibouti", "Egypt", "Equatorial Guinea", "Eritrea", "Ethiopia", "Gabon", "Gambia", "Ghana", "Guinea", "Guinea-Bissau", "Kenya", "Lesotho", "Liberia", "Libya", "Madagascar", "Malawi", "Mali", "Mauritania", "Mauritius", "Morocco", "Mozambique", "Namibia", "Niger", "Nigeria", "Rwanda", "Sao Tome and Pricipe", "Senegal", "Seychelles", "Sierra Leone", "Somalia", "South Africa", "South Sudan", "Sudan", "Swaziland", "Tanzania", "Togo", "Tunisia", "Uganda", "Zambia", "Zimbabwe"};
 		private static Scanner in;
 	}
