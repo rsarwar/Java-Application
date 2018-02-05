f46383288b538fb26e0d17a3927aef7386572e12
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2018-02-05 13:07:46 -0500

    Edits to TourGuide runner and utilities

--- All Around the World of Africa/src/Utilities.java
+++ All Around the World of Africa/src/Utilities.java
@@ -9,33 +9,3 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage; 
-
-public class Utilities {
-
-	public static void setIcon()
-	{
-		Image image;
-		try {
-			image = new Image(new FileInputStream("wahoorat.png"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		ImageView imageView = new ImageView(image);
-		imageView.setX(300);
-		imageView.setY(400);
-		
-		//have the icon greet the user
-	}
-	
-	public static void hoverOverCountry()
-	{
-		
-	}
-	
-	public static void showData()
-	{
-		
-	}
-	
-}
