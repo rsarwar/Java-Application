83716f62b4ec1a684252a620a3da444c09df01e6
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2018-02-06 13:13:39 -0500

    FSAF

--- All Around the World of Africa/src/RunnerStuff/Runner.java
+++ All Around the World of Africa/src/RunnerStuff/Runner.java
@@ -67,7 +67,7 @@ public class Runner extends Application{
 		button1.getButton().addEventHandler(MouseEvent.MOUSE_CLICKED,
 		        new EventHandler<MouseEvent>() {
 		          @Override
-		          //FAEF
+		          //FAEFDSAD
 		          public void handle(MouseEvent e) {
 		        	  StackPane root1 = new StackPane();
 		        	  Scene scene1 = new Scene(root1, 830,800);
