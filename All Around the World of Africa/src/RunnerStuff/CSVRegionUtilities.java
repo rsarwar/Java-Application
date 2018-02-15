0ffd5323823fd53cf19336e2d79aa397dfb8f7c3
togekissou <togekissou@192.168.2.9>
2018-02-14 21:36:07 -0500

    ugh

---
+++ All Around the World of Africa/src/RunnerStuff/CSVRegionUtilities.java
@@ -0,0 +1,123 @@
+package RunnerStuff;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Miriam Monroe and Ramisha Sarwar
+ */
+public class CSVRegionUtilities implements CSVUtilities
+{
+	ArrayList<String> CSVUserData = new ArrayList<String>();
+	int numColumns = 2;
+	int numRows = 0;
+	
+	
+	public void CSVUtilities(File csv)
+	{
+			FileReader reader = null;
+			BufferedReader filein = null;
+			try
+			{
+				reader = new FileReader(csv);
+				filein = new BufferedReader(reader);
+				try
+				{
+					String line = filein.readLine();
+					
+						while (line != null)
+						{ 						
+							String[] attributes = line.split(",");
+							//numColumns = attributes.length;
+							for(int x = 0; x < numColumns; x++ )
+							{
+								CSVUserData.add(attributes[x]);
+							}
+							numRows++;
+							line = filein.readLine(); 
+						}
+				}
+				
+				catch (IOException ioe)
+				{
+		            ioe.printStackTrace();
+		        }	
+			}
+
+		catch (FileNotFoundException fnfe)
+		{
+			System.out.println("ERROR: File not found!");
+		}
+
+	} 
+	
+	public void printData()
+	{
+		int y = this.numColumns;
+		int z = this.numRows;
+		int a = 0;
+		int b = y * z;
+		while(a < b)
+		{
+			for(int i = 0; i < y; i++)
+			{	
+				if(a < CSVUserData.size())
+				{
+					System.out.print(this.CSVUserData.get(a) + "  ");					
+				}
+				a++;
+			}
+			System.out.println();
+			
+		}
+	}
+	
+	public List<String> getDataString(int column)
+	{
+		ArrayList<String> x = new ArrayList<String>();
+		int i = column+numColumns;
+		while(i < numColumns*numRows-1)
+		{	
+			String y = CSVUserData.get(i);
+			x.add(y);
+			i = i+numColumns;
+		}
+		return x;
+		 
+	}
+
+	public List<Integer> getDataInt(int column)
+	{
+		ArrayList<Integer> x = new ArrayList<Integer>();
+		int i = column+numColumns;
+		while(i < numColumns*numRows)
+		{	
+			int y = Integer.parseInt(CSVUserData.get(i));
+			x.add(y);
+			i = i+numColumns;
+		}
+		return x;
+	}
+	
+	@Override
+	public List<Double> getDataDouble(int column)
+	{
+		ArrayList<Double> x = new ArrayList<Double>();
+		int i = column+numColumns;
+		while(i < numColumns*numRows)
+		{	
+			double y = Double.parseDouble(CSVUserData.get(i));
+			x.add(y);
+			i = i+numColumns;
+		}
+		return x;
+	}
+
+	
+}
