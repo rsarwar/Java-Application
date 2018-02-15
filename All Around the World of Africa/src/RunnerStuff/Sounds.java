6e63f31fee144770fc49664b73c2bdfe639e6bdf
lamia <lamia@DESKTOP-H98TGEO.home>
2018-02-15 03:06:03 -0500

    Finished for tonight

--- All Around the World of Africa/src/RunnerStuff/Sounds.java
+++ All Around the World of Africa/src/RunnerStuff/Sounds.java
@@ -9,29 +9,8 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
-public class Sounds /*extends Application*/ {
+public class Sounds {
 
-	
-  /*public static void main(String[] args) {
-    launch(args);
-  }
-
-  @Override
-  public void start(Stage primaryStage) {
-	String path = "C:\\Users\\BT_1N3_06\\git\\Java-Application\\All Around the World of Africa\\src\\name.mp3";
-	  
-    //final URL resource = getClass().getResource("name.mp3");
-    final Media media = new Media(new File(path).toURI().toString());
-    
-    final MediaPlayer mediaPlayer = new MediaPlayer(media);
-    
-    mediaPlayer.play();
-
-    primaryStage.setTitle("Audio Player 1");
-    primaryStage.setWidth(200);
-    primaryStage.setHeight(200);
-    primaryStage.show();
-  }*/
 	
 	private String path;
 	
